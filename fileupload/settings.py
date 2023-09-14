a6a4d78d6adce47003be7b3ff0a16c116c425c5f
sharanbrar <sharanbrar@gmail.com>
2023-09-14 19:35:14 +0530

    changes

--- fileupload/settings.py
+++ fileupload/settings.py
@@ -134,15 +134,16 @@ import os
 STATIC_URL = '/static/'
 
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIR = {
+    os.path.join(BASE_DIR , "public/static")
+}
 # STATIC_ROOT = 'staticfiles'
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-STATICFILES_DIR = {
-    os.path.join(BASE_DIR , "public/static")
-}
+
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'public/static') 
 MEDIA_URL = '/media/'
